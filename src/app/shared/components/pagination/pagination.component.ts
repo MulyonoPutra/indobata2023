9eab237cc348b1e8b31c37457638149b6b2aa58a
Mulyono Putra <mulyonoputra1994@gmail.com>
2023-06-20 04:36:46 +0700

    feat: reusable pagination component

--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -6,14 +6,13 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 	styleUrls: ['./pagination.component.scss'],
 })
 export class PaginationComponent {
-	@Input() currentPage!: number;
-	@Input() totalPages!: number;
-	@Input() limit!: number;
-	@Input() totalItems!: number;
+  @Input() page!: number;
+  @Input() totalItems!: number;
 
 	@Output() pageChange: EventEmitter<number> = new EventEmitter<number>();
 
-	onPageChange(pageNumber: any): void {
+	onPageChanged(pageNumber: any): void {
 		this.pageChange.emit(pageNumber);
 	}
+
 }
