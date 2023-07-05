7e67f09f78451882a8c7940f1eaa017ede478ddc
Mulyono Putra <mulyonoputra1994@gmail.com>
2023-07-05 11:08:21 +0700

    add capitalize

--- src/app/modules/profile/profile-view/profile.module.ts
+++ src/app/modules/profile/profile-view/profile.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
 import { LazyLoadImageModule } from 'ng-lazyload-image';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
 	declarations: [ProfileComponent],
-	imports: [CommonModule, ProfileRoutingModule, LazyLoadImageModule],
+	imports: [CommonModule, ProfileRoutingModule, LazyLoadImageModule, SharedModule],
 })
 export class ProfileModule {}
